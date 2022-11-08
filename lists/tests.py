90e478deccc288e69ac73d885360a4709c094057
brianmakns <brian@valos.ai>
2022-11-08 16:37:29 +0000

    used django client to test- much simpler

--- lists/tests.py
+++ lists/tests.py
@@ -4,15 +4,9 @@ from django.urls import resolve
 from django.template.loader import render_to_string
 from lists.views import home_page
 
-class SmokeTest(TestCase): 
-    
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/') # checking that resolve() finds a function called home_page 
-        self.assertEqual(found.func, home_page)
+class HomePageTest(TestCase): 
     
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest() 
-        response = home_page(request) 
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html') # instead of testing constants, testing implementation. 
+# 
