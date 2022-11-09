90c54a39efdfe97e1a6a41c0788dedc49e209d5e
brianmakns <brian@valos.ai>
2022-11-09 11:24:17 +0000

    showing one added item on site. To start DB

--- lists/views.py
+++ lists/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html') # render() builds HttpResponse 
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', '') # the name attribute of an <input> tag 
+    }) # render() builds HttpResponse 
