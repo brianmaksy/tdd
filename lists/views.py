ea1632cc9d922c3ba5ed5137e439e8f97acc3881
brianmakns <brian@valos.ai>
2022-11-09 12:45:37 +0000

    redirect after POST, and show all DB-retrieved items in template

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text', '') # the name attribute of an <input> tag 
-    }) # render() builds HttpResponse 
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text']) # initialise Item() and .save() in one step 
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
+
